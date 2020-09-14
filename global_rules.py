8bf6f4c91edd30196fe678d9736cec7557a59bf0
ZacharyMass <zm.zach98@gmail.com>
2020-09-13 19:17:48 -0500

    Added image attachment argument to send_message

--- global_rules.py
+++ global_rules.py
@@ -5,12 +5,12 @@ def run(data, bot_info, send):
     message = data['text']
 
     if message == '.help':
-        send(help_message, bot_info[0])
+        send(help_message, bot_info[0], "")
         return True
 
     if message == '.test':
-        send("Hi there! Your bot is working, you should start customizing it now.", bot_info[0])
+        send("Hi there! Your bot is working, you should start customizing it now.", bot_info[0], "")
         return True
 
-    send("Hi {}! You said: {}".format(data['name'], data['text']), bot_info[0])
+    send("Hi {}! You said: {}".format(data['name'], data['text']), bot_info[0], "")
     return True
